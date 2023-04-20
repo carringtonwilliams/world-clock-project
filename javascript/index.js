349324e7b2010b141a17a6c533bfbe97e967ec19
Carrington Williams <carringtonawilliams@gmail.com>
2023-04-19 21:08:09 -0700

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -25,11 +25,14 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
+  let citiesElement = document.querySelector("#city");
   citiesElement.innerHTML = ` 
-  <div class="cities">
+  <div class="city">
         <div>
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
@@ -44,5 +47,5 @@ function updateCity(event) {
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("#cities");
+let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
